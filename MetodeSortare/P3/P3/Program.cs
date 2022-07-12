72364c42536c05d4b067e8bf6e3768bd77e893a2
Vinter Andrada <vinterandrada08@gmail.com>
2022-07-12 21:33:44 +0300

    finished

---
+++ MetodeSortare/P3/P3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P3
+{
+    class Program
+    {
+        /// <summary>
+        /// Se consideră fişierul BAC.TXT ce conţine cel mult un milion de numere naturale separate prin spatii, fiecare număr având 
+        /// cel mult nouă cifre. Scrieţi un program C# care citeşte toate numerele din fişierul BAC.TXT şi determină, folosind un algoritm 
+        /// eficient din punct de vedere timpului de executare, cele mai mari două numere de trei cifre care nu se află în fişier. 
+        /// Cele două numere vor fi afişate pe ecran în ordine descrescătoare, cu un spaţiu între ele. Dacă nu pot fi determinate două astfel 
+        /// de numere, programul va afişa pe ecran valoarea 0. 
+        /// Exemplu: dacă fişierul BAC.TXT conţine numerele: 12 2345 123 67 989 6 999 123 67 989 999, atunci programul va afişa 998 997
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
