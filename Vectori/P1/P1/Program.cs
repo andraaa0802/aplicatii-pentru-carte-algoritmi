ac2425648a32057a2a35c4d03d712aeca524a9e8
Vinter Andrada <vinterandrada08@gmail.com>
2022-07-12 21:14:14 +0300

    finished

---
+++ Vectori/P1/P1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+namespace P1
+{
+    class Program
+    {
+        /// <summary>
+        /// Să se formeze vectorul primelor n, unde n este un număr natural dat, elemente ale şirului lui Fibonacci: 
+        /// 0, 1, 1, 2, 3, 5, 8, 13, 21,.... (f[1]=0, f[2]=1, f[i]=f[i-1]+f[i-2]) și să se afișeze în fișierul fibonacci.out 
+        /// </summary>
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\Users\CRISTI\Desktop\AplicatiiCarte\Vectori\P1\fibonacci.out");
+            int n = int.Parse(Console.ReadLine());
+            int[] f = new int[n];
+            int nr1 = 0, nr2 = 1; 
+            if (n == 1) //daca n este 1, vectorul il va contine doar pe 0, acesta fiind primul element al sirului lui Fibonacci
+                f[0] = nr1;
+            else if(n==2) //daca n este 2, vectorul va contine 0 si 1, fiind primele 2 elemente ale sirului
+            {
+                f[0] = nr1;
+                f[1] = nr2;
+            }
+            else //daca n este mai mare decat 2, incepand cu elementul al 3-lea, elementele se vor forma prin adunarea celor 2 numere precedente
+            {
+                f[0] = nr1;
+                f[1] = nr2;
+                int nr3;
+                for(int i=2;i<n;i++)
+                {
+                    nr3 = nr1 + nr2;
+                    f[i] = nr3;
+                    nr1 = nr2;
+                    nr2 = nr3; 
+                }
+            }
+            for (int i = 0; i < n; i++)
+                sw.Write(f[i] + " ");
+            sw.Close();
+        }
+    }
+}
---
+++ Vectori/P2/P2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace P2
+{
+    class Program
+    {
+        /// <summary>
+        /// Fișierul date.in conține numere naturale mai mici decât 100. Se cere să se afișeze, în ordine crescătoare elementele aflate 
+        /// în fișier. Indicație: se va crea un tablou, inițializat cu zero, care “numără” elementele din fișier. Astfel, fiecare număr din 
+        /// fișier este considerat indice al tabloului și, de fiecare data când este extrase, valoarea din tablou este incrementată (v[x]=v[x]+1)
+        /// </summary>
+        
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\CRISTI\Desktop\AplicatiiCarte\Vectori\P2\date.in");
+            string[] buffer = sr.ReadLine().Split(' ');
+            int lungime = buffer.Length; //lungimea vectorului este egala cu lungimea stringului citit de pe prima linie
+            int[] v_initial = new int[lungime];
+            int maxim = -1; //avem nevoie sa gasim maximul pentru a stabili lungimea vectorului ce trebuie creat
+            
+            for(int i=0;i<lungime;i++)
+            {
+                v_initial[i] = int.Parse(buffer[i]);
+                if (v_initial[i] > maxim)
+                    maxim = v_initial[i];
+            }
+            int[] v = new int[maxim + 1];
+            for(int i=0;i<lungime;i++)
+                v[v_initial[i]]++; //numaram aparitiile numerelor din vectorul initial
+            for(int i=0;i<maxim+1;i++)
+                if(v[i]!=0)
+                    while (v[i]!=0) //pentru numerele care apar de mai multe ori, le vom afisa de cate ori apar
+                    {
+                        Console.Write(i+" ");
+                        v[i]--;
+                    }
+                    
+        }
+    }
+}
