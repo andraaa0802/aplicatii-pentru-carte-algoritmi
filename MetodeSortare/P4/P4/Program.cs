6011de385cdd31f393d2f5b0595ce246cee7fc7d
Vinter Andrada <vinterandrada08@gmail.com>
2022-07-13 00:20:05 +0300

    finished

---
+++ MetodeSortare/P4/P4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace P4
+{
+    class Program
+    {
+        /// <summary>
+        /// În fişierul numere.txt sunt memorate maximum 10000 de numere naturale cu cel mult 9 cifre fiecare.
+        /// Fiecare linie a fişierului conţine câte un număr. Se cere afişarea pe ecran, în ordine descrescrescătoare, a tuturor cifrelor 
+        /// care apar în numerele din fişier. Alegeţi un algoritm de rezolvare eficient din punct de vedere al timpului de executare.
+        /// Exemplu: dacă fişierul numere.txt conţine:267 39628 79, se va tipări 9987766322.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            int[] cif = new int[10];//vector de frecventa pt cifrele numerelor din fisier
+            foreach (string line in File.ReadLines(@"C:\Users\CRISTI\Desktop\AplicatiiCarte\MetodeSortare\P4\numere.txt"))
+            {
+                int nr = int.Parse(line);
+                while (nr!=0)
+                {
+                    cif[nr % 10]++; //stocheaza frecventa aparitiei cifrelor din nr, unde nr este numarul citit din fisier
+                    nr /= 10;
+                }
+            }
+            for(int i=9;i>=0;i--)
+                if(cif[i]!=0)
+                    while(cif[i]!=0) //pentru a afisa fiecare cifra de cate ori apare
+                    {
+                        Console.Write(i);
+                        cif[i]--;
+                    }
+        }
+    }
+}
---
+++ MetodeSortare/P5/P5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace P5
+{
+    class Program
+    {/// <summary>
+     /// În fişierul numere.txt pe prima linie este memorat un număr natural n (n≤10000), iar pe linia următoare un şir de n numere naturale 
+     /// distincte două câte două, separate prin câte un spaţiu, cu maximum 4 cifre fiecare. Se cere afişarea pe ecran a poziţiei pe care s-ar
+     /// găsi primul element din şirul aflat pe linia a doua a fişierului, în cazul în care şirul ar fi ordonat crescător.
+     /// Numerotarea poziţiilor elementelor în cadrul şirului este de la 1 la n. Alegeţi un algoritm de rezolvare eficient din punct de vedere 
+     /// al memoriei utilizate şi al timpului de executare. Exemplu: dacă fişierul numere.txt conţine: 6 267 13 45 628 7 79, se va afişa 5, 
+     /// deoarece primul element din şirul iniţial, 267, s-ar găsi pe poziţia a cincea în şirul ordonat crescător (7 13 45 79 267 628).
+     /// </summary>
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\CRISTI\Desktop\AplicatiiCarte\MetodeSortare\P5\Numere.txt");
+            int n = int.Parse(sr.ReadLine());
+            int[] v = new int[n]; 
+            string[] buffer = sr.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+                v[i] = int.Parse(buffer[i]);
+            int nr1 = v[0]; //retin primul numar din sir
+            //sortez crescator sirul
+            for(int i=0;i<n-1;i++)
+                for(int j=i+1;j<n;j++)
+                    if(v[i]>v[j])
+                    {
+                        int aux = v[i];
+                        v[i] = v[j];
+                        v[j] = aux;
+                    }
+            //caut nr1 in vectorul ordonat si afisez pozitia la care a fost gasit
+            for(int i=0;i<n;i++)
+                if(v[i]==nr1)
+                {
+                    Console.WriteLine(i+1); //adaug 1 la i pentru a pastra numerotarea de la 1 la n
+                    break;
+                }
+        }
+    }
+}
